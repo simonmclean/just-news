fda5a64884172448f7923d49c51c4fa7796e952d
simonmclean <simonmclean1988@gmail.com>
2020-03-26 17:29:12 +0000

    cleanup

--- utils/functionUtils.js
+++
@@ -1,9 +0,0 @@
-/*
- * Isomorphic wrapper for AbortController
- * Uses a mock implementation when run on the server
- */
-export function isoAbortController() {
-    return typeof window !== 'undefined'
-        ? AbortController
-        : { abort: () => {} }
-}
