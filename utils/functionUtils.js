7bf7251798ff9114eb68262814bf3d5b54c1b2ae
simonmclean <simonmclean1988@gmail.com>
2020-03-29 12:31:21 +0100

    isometric abort controller

---
+++ utils/functionUtils.js
@@ -0,0 +1,8 @@
+export function isoAbortController() {
+    return typeof window !== 'undefined'
+        ? new AbortController()
+        : {
+            abort: () => {},
+            signal: {},
+        }
+}
