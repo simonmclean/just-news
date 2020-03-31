e0fc394dca658eaec7be1da7f6f07ea8cdbfd1de
simonmclean <simonmclean1988@gmail.com>
2020-03-31 18:11:37 +0100

    Styling for story card with image

--- components/stories/StoryCard/StoryCard.js
+++ components/stories/StoryCard/StoryCard.js
@@ -4,18 +4,31 @@ import css from "./StoryCard.module.css";
 const secureURL = (url) => url.replace("http://", "https://");
 
 export default function StoryCard({ story, showImage }) {
+    const canShowImage = showImage && story.urlToImage;
+
+    const wrapperClass = `${css.card} ${canShowImage && css.cardWithImage}`;
+
+    const renderAttribution = () => (
+        <div className={css.sourceTime}>
+            <cite className={css.source}>{story.source.name}</cite>
+            <time className={css.dateTime} dateTime={story.publishedAt}>
+                {story.relativeTime}
+            </time>
+        </div>
+    )
+
     return (
-        <article className={css.card}>
-            {showImage && story.urlToImage && (
-                <lazy-img src={secureURL(story.urlToImage)}>
-                    <img loading="lazy" alt={story.title} />
-                </lazy-img>
+        <article className={wrapperClass}>
+            {canShowImage && (
+                <>
+                    {renderAttribution()}
+                    <lazy-img src={secureURL(story.urlToImage)}>
+                        <img className={css.image} loading="lazy" alt={story.title} />
+                    </lazy-img>
+                </>
             )}
-            <header>
-                <cite className={css.source}>{story.source.name}</cite>
-                <time className={css.dateTime} dateTime={story.publishedAt}>
-                    {story.relativeTime}
-                </time>
+            <header className={css.header}>
+                {!canShowImage && renderAttribution()}
                 <h3 className={css.title}>
                     <a
                         className={css.title}
@@ -27,7 +40,7 @@ export default function StoryCard({ story, showImage }) {
                     </a>
                 </h3>
             </header>
-            <section>
+            <section className={css.section}>
                 <p className={css.description}>{story.description}</p>
             </section>
         </article>
