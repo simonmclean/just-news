277afdd3ec6934a3a7ff6a906e2d8e8a16b381f8
simonmclean <simonmclean1988@gmail.com>
2020-04-09 16:01:38 +0100

    Increase default page size to 50

--- utils/requestUtils.js
+++ utils/requestUtils.js
@@ -5,7 +5,7 @@ import { upperFirst } from "./functionUtils";
 // TODO: Remove
 const API_KEY = process.env.newsApiKey;
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 50;
 
 const ENDPOINTS = {
     SOURCES: "sources",
