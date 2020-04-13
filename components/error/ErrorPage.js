d2f5ead1b5ce018085704b94bd2cfce5bec68ed7
simonmclean <simonmclean1988@gmail.com>
2020-04-13 12:11:44 +0100

    Error page

---
+++ components/error/ErrorPage.js
@@ -0,0 +1,23 @@
+import React from "react";
+import css from "./ErrorPage.module.css";
+
+function ErrorPage({ statusCode }) {
+    return (
+        <div className={css.errorPage}>
+            {statusCode && <p>An error {statusCode} occured on the server.</p>}
+            {!statusCode && (
+                <>
+                    <p>An error occured on the client.</p>
+                    <p>Please open an issue on <a href="https://github.com/simonmclean/just-news/issues/new" target="_blank" rel="noopener noreferrer">Github</a> and include steps to reproduce.</p>
+                </>
+            )}
+        </div>
+    )
+}
+
+ErrorPage.getInitialProps = ({ res, err }) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+    return { statusCode }
+}
+
+export default ErrorPage;
---
+++ pages/_error.js
@@ -0,0 +1,3 @@
+import ErrorPage from "../components/error/ErrorPage";
+
+export default ErrorPage
